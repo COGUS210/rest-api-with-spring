bf48d8a8348f0e84f0f34f047dd7d74b526f5bf7
COGUS210 <43839577+COGUS210@users.noreply.github.com>
2021-01-20 22:34:49 +0900

    Account 도메인 추가 (스프링 시큐리티 적용)

--- rest-api-with-spring/src/test/java/com/example/demoinflearnrestapi/DemoApplicationTests.java
+++ rest-api-with-spring/src/test/java/com/example/demoinflearnrestapi/DemoApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example.demoinflearnrestapi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DemoInflearnRestApiApplicationTests {
+class DemoApplicationTests {
 
     @Test
     void contextLoads() {
